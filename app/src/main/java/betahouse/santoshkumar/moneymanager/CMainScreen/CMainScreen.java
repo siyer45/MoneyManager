31950b3f9a7193162213b640e8292e8ac5d73db0
Santoshkumar <siyer45@github.com>
2015-06-01 01:31:34 +0530

    initial drawer implementation

--- app/src/main/java/betahouse/santoshkumar/moneymanager/CMainScreen/CMainScreen.java
+++ app/src/main/java/betahouse/santoshkumar/moneymanager/CMainScreen/CMainScreen.java
@@ -1,12 +1,19 @@
 package betahouse.santoshkumar.moneymanager.CMainScreen;
 
 import android.app.ActionBar;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import betahouse.santoshkumar.moneymanager.R;
 
@@ -16,19 +23,90 @@ import betahouse.santoshkumar.moneymanager.R;
 
 public class CMainScreen extends ActionBarActivity
 {
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private final String[] Friend_List = { "Naman", "Santosh", "Jawed", "Pawan", "Nadeem" };
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        android.support.v7.app.ActionBar ActionBar =getSupportActionBar();
-
-        ActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        ActionBar.setCustomView(R.layout.actionbar_layout);
-
+        android.support.v7.app.ActionBar ActionBar = getSupportActionBar();
         ActionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00b0f0")));
 
 
         setContentView(R.layout.mainscreen_layout);
+
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+        addDrawerItems();
+        setupDrawer();
+
+        ActionBar.setDisplayHomeAsUpEnabled(true);
+        ActionBar.setHomeButtonEnabled(true);
+        ActionBar.setCustomView(R.layout.actionbar_layout);
+    }
+
+
+    private void addDrawerItems()
+    {
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Friend_List);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener
+                (
+                        new AdapterView.OnItemClickListener()
+                        {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+                            {
+                                mDrawerLayout.closeDrawers();
+                            }
+                        }
+                );
+    }
+
+    private void setupDrawer()
+    {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close)
+        {
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView)
+            {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view)
+            {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -49,6 +127,12 @@ public class CMainScreen extends ActionBarActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item))
         {
             return true;
         }
